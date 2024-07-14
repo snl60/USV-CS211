1d102a805520a13d844bfec7292f45c59c5fabec
snl60 <157643836+snl60@users.noreply.github.com>
2024-07-14 14:19:26 -0700

    Create ISuperSpeed.h

---
+++ Week10/ISuperSpeed.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+
+Scott Lopez
+Professor Gale
+CS211 - Code 1: Intermediate Programming
+July 14, 2024
+
+Homework Week 10 -- ISuperSpeed.h
+
+*******************************************************************************/
+
+#pragma once
+
+class ISuperSpeed {
+public:
+	virtual void superSpeed() = 0;
+};
