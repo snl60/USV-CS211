f9ddf2b65d17fad887e0e36a1915fde879b6441f
snl60 <157643836+snl60@users.noreply.github.com>
2024-07-14 14:18:30 -0700

    Create ISeaCreatureCommunication.h

---
+++ Week10/ISeaCreatureCommunication.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+
+Scott Lopez
+Professor Gale
+CS211 - Code 1: Intermediate Programming
+July 14, 2024
+
+Homework Week 10 -- ISeaCreatureCommunication.h
+
+*******************************************************************************/
+
+#pragma once
+
+class ISeaCreatureCommunication {
+public:
+	virtual void seaCreatureCommunication() = 0;
+};
