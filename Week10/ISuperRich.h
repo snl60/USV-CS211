567199fd7f4fe8859bbf2b560638fd1bb7e4ab17
snl60 <157643836+snl60@users.noreply.github.com>
2024-07-14 14:19:06 -0700

    Create ISuperRich.h

---
+++ Week10/ISuperRich.h
@@ -0,0 +1,18 @@
+/*******************************************************************************
+
+Scott Lopez
+Professor Gale
+CS211 - Code 1: Intermediate Programming
+July 14, 2024
+
+Homework Week 10 -- ISuperRich.h
+
+*******************************************************************************/
+
+#pragma once
+
+class ISuperRich {
+public:
+	virtual void superRich() = 0;
+	virtual void gadgets() = 0;
+};
