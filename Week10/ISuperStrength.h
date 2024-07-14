844b06516ce007ab19ea0c1d2f9fadb24f3da117
snl60 <157643836+snl60@users.noreply.github.com>
2024-07-14 14:19:43 -0700

    Create ISuperStrength.h

---
+++ Week10/ISuperStrength.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+
+Scott Lopez
+Professor Gale
+CS211 - Code 1: Intermediate Programming
+July 14, 2024
+
+Homework Week 10 -- ISuperStrength.h
+
+*******************************************************************************/
+
+#pragma once
+
+class ISuperStrength {
+public:
+	virtual void superStrength() = 0;
+};
