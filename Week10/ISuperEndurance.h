b4c216fc0e80cac63b9a2251b47273db1bb3bb36
snl60 <157643836+snl60@users.noreply.github.com>
2024-07-14 14:18:46 -0700

    Create ISuperEndurance.h

---
+++ Week10/ISuperEndurance.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+
+Scott Lopez
+Professor Gale
+CS211 - Code 1: Intermediate Programming
+July 14, 2024
+
+Homework Week 10 -- ISuperEndurance.h
+
+*******************************************************************************/
+
+#pragma once
+
+class ISuperEndurance {
+public:
+	virtual void superEndurance() = 0;
+};
