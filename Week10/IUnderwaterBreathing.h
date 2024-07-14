01d019312ce6cc023a2f877410bd861af47a6d61
snl60 <157643836+snl60@users.noreply.github.com>
2024-07-14 14:20:17 -0700

    Create IUnderwaterBreathing.h

---
+++ Week10/IUnderwaterBreathing.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+
+Scott Lopez
+Professor Gale
+CS211 - Code 1: Intermediate Programming
+July 14, 2024
+
+Homework Week 10 -- IUnderwaterBreathing.h
+
+*******************************************************************************/
+
+#pragma once
+
+class IUnderwaterBreathing {
+public:
+	virtual void underwaterBreathing() = 0;
+};
