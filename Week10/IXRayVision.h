e2ad893dc1ed226832e59fb031ec2e8db1c21a3f
snl60 <157643836+snl60@users.noreply.github.com>
2024-07-14 14:20:36 -0700

    Create IXRayVision.h

---
+++ Week10/IXRayVision.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+
+Scott Lopez
+Professor Gale
+CS211 - Code 1: Intermediate Programming
+July 14, 2024
+
+Homework Week 10 -- IXRayVision.h
+
+*******************************************************************************/
+
+#pragma once
+
+class IXRayVision {
+public:
+	virtual void xRayVision() = 0;
+};
