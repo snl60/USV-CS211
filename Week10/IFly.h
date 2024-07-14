099542c1a8f0cfa2ca009f671b942839dbdcf8e5
snl60 <157643836+snl60@users.noreply.github.com>
2024-07-14 14:18:10 -0700

    Create IFly.h

---
+++ Week10/IFly.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+
+Scott Lopez
+Professor Gale
+CS211 - Code 1: Intermediate Programming
+July 14, 2024
+
+Homework Week 10 -- IFly.h
+
+*******************************************************************************/
+
+#pragma once
+
+class IFly {
+public:
+	virtual void fly() = 0;
+};
