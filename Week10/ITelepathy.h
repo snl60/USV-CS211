1e083893f81d4edaa53006a856cb72778881873b
snl60 <157643836+snl60@users.noreply.github.com>
2024-07-14 14:20:02 -0700

    Create ITelepathy.h

---
+++ Week10/ITelepathy.h
@@ -0,0 +1,18 @@
+/*******************************************************************************
+
+Scott Lopez
+Professor Gale
+CS211 - Code 1: Intermediate Programming
+July 14, 2024
+
+Homework Week 10 -- ITelepathy.h
+
+*******************************************************************************/
+
+#pragma once
+
+class ITelepathy {
+public:
+	virtual void mindControl() = 0;
+	virtual void mindReading() = 0;
+};
